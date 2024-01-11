49dd7c12cfbbf896cbe6ca743805cb8fc6580386
Morty Cai <morty.cai@assaabloy.com>
2024-01-11 11:33:27 +0800

    fix: fix a all toggle state issue

--- src/Components/Todo List/Header/index.jsx
+++ src/Components/Todo List/Header/index.jsx
@@ -6,8 +6,13 @@ import {
 import TodoHeader from "./TodoHeader"
 
 const mapStateToProps = (state) => {
-    const completedCount = state.todos.todos.filter((todo) => todo.isCompleted).length
     const allCount = state.todos.todos.length
+    if (allCount == 0) {
+        return {
+            isAllCompleted: false
+        }
+    }
+    const completedCount = state.todos.todos.filter((todo) => todo.isCompleted).length
     return {
         isAllCompleted: completedCount == allCount
     }
